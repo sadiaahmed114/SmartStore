c559d94d2ab2036ef821f753f8bb9ecc32d492f7
sadiaahmed114 <sadiaahmed26373@gmail.com>
2024-05-28 02:08:23 -0700

    FINAL

--- smart_website/app/checkout/page.tsx
+++ smart_website/app/checkout/page.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 
 const CheckoutPage = () => {
     return (
-    
-        <div className=' flex justify-center items-center'>
-            <h1  className="font-bold text-3xl text-red-900 text-center">Welcome to  My Checkout Page</h1>
+    // <><div className='absolute left-9 w-72 h-72 bg-red-400 rounded-full'></div>
+    // <div className='absolute left-9 w-72 h-72 bg-yellow-400 rounded-full'></div></>
+        <div className=' h-96 flex justify-center items-center'>
+            <div className='absolute w-72 h-72 bg-red-400 rounded-full  z-10'></div>
+            <div className='absolute right-2rem w-72 h-72 bg-yellow-400 rounded-full z-20'></div>
+            {/* <div className='absolute left-9 w-72 h-72 bg-green-400 rounded-full'></div> */}
+            {/* <h1  className="font-bold text-4xl text-center">Welcome to  My Checkout Page</h1> */}
         </div>
+  
+
         
     );
 };
