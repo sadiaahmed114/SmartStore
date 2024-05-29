ed010b64664e26409fd29916a606b61ef141ffcd
sadiaahmed114 <sadiaahmed26373@gmail.com>
2024-05-28 17:41:50 -0700

    FINAL

--- smart_website/app/checkout/page.tsx
+++ smart_website/app/checkout/page.tsx
@@ -1,12 +1,14 @@
-
+import Image from 'next/image';
 import React from 'react';
 
 const CheckoutPage = () => {
     return (
-        <div className=' h-96 flex justify-center items-center'>
-            <h1  className="font-bold text-4xl text-center">Welcome to  My Checkout Page</h1>
-        </div>    
+        
+            <div className='h-96 flex justify-center items-center'>
+                    <h1  className="font-bold text-white text-4xl text-center">Welcome to  My Checkout Page</h1>
+            </div>    
     );
 };
 
 export default CheckoutPage;
+
--- smart_website/stripe/error/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorStripe(){
-    return (
-        <div className="py-10">
-            <h2>Something went wrong.........</h2>
-        </div>
-    )
-}
